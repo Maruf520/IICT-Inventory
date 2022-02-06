e57be2e3f4a15206bc39c2c475ce15ca8f674cb7
maruf520 <md.maruf5201@gmail.com>
2022-02-06 10:21:38 +0600

    Migration Added

--- src/IICT-Store.Api/Migrations/Identity/20220206042046_latest.cs
+++ src/IICT-Store.Api/Migrations/Identity/20220206042046_latest.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace IICT_Store.Api.Migrations.Identity
 {
-    public partial class initial : Migration
+    public partial class latest : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
