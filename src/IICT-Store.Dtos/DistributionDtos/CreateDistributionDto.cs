702a0fd9112468efb01847245b22e5afdddbe73e
maruf520 <md.maruf5201@gmail.com>
2022-02-16 02:42:08 +0600

    Distribution field required

--- src/IICT-Store.Dtos/DistributionDtos/CreateDistributionDto.cs
+++ src/IICT-Store.Dtos/DistributionDtos/CreateDistributionDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,17 @@ namespace IICT_Store.Dtos.DistributionDtos
     public class CreateDistributionDto
     {
         public int RoomNo { get; set; }
+        [Required]
         public long ProductId { get; set; }
+        [Required]
         public int Quantity { get; set; }
         public string NameOfUser { get; set; }
         public string Description { get; set; }
+        [Required]
         public int OrderNo { get; set; }
+        [Required]
         public long SenderId { get; set; } //rakesh account reference
+        [Required]
         public long ReceiverId { get; set; }
         public ICollection<ProductSerialNoDto> ProductSerialNo { get; set; }
         public int DistributedTo { get; set; } // to samir sir/ room no 212
